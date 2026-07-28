ed5d29a360e33c5671be2d972ed406ae63e37549
Siddarth Shinde <siddarthshinde06@gmail.com>
2026-07-28 19:55:37 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -113,4 +113,6 @@ def health_guide():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    debug = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
+    app.run(host="0.0.0.0", port=port, debug=debug)
